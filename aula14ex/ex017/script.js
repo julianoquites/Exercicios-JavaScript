fcd0022a4565172eed336b40dcac1aa565877c51
Juliano Quites <juliano.quites@gmail.com>
2022-01-25 15:24:01 -0300

    Contador

---
+++ aula14ex/ex017/script.js
@@ -0,0 +1,30 @@
+function contar() {
+    let inicio = window.document.getElementById('ninicio')
+    let fim = window.document.getElementById('nfim')
+    let passo = window.document.getElementById('npasso')
+    let res = window.document.getElementById('res')
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if ( p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            // Contagem crescente
+            for(let c = i; c <= f; c+= p) {
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        } else {
+            // Contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
